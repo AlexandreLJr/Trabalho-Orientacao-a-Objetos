5514e1869850ba748ee7641f03a4c1d765630bdb
AlexandreLJr <ubc183@gmail.com>
2023-07-05 23:00:45 -0300

    teste de commit

--- src/entidades/Acesso.java
+++ src/entidades/Acesso.java
@@ -2,4 +2,7 @@ package entidades;
 
 public class Acesso {
 
+	private Estacionamento estacionamento = new Estacionamento();
+	private String placa;
+	
 }
--- src/entidades/Estacionamento.java
+++ src/entidades/Estacionamento.java
@@ -1,5 +1,49 @@
 package entidades;
 
-public class Estacionamento {
+import java.time.LocalDate;
 
+public class Estacionamento {
+	
+	private int id;
+	private LocalDate horarioFuncionamento;
+	private int capacidade;
+	
+	public Estacionamento() {
+	}
+	
+	public Estacionamento(int id, LocalDate horarioFuncionamento, int capacidade) {
+		super();
+		this.id = id;
+		this.horarioFuncionamento = horarioFuncionamento;
+		this.capacidade = capacidade;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDate getHorarioFuncionamento() {
+		return horarioFuncionamento;
+	}
+
+	public void setHorarioFuncionamento(LocalDate horarioFuncionamento) {
+		this.horarioFuncionamento = horarioFuncionamento;
+	}
+
+	public int getCapacidade() {
+		return capacidade;
+	}
+
+	public void setCapacidade(int capacidade) {
+		this.capacidade = capacidade;
+	}
+	
+	public boolean calcularCapacidade(int capacidade) {
+		return true;
+	}
+	
 }

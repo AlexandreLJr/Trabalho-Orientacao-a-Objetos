63b02fbc41c15dfe54626d488a58d607a3a966c7
Felipe Freire <62055315+gandal-fe@users.noreply.github.com>
2023-07-07 22:32:30 -0300

    add class contratante

---
+++ src/entidades/Contratante.java
@@ -0,0 +1,23 @@
+package entidades;
+
+public class Contratante {
+	
+	private int retContratante;
+	
+	public float valorContratante(){
+			
+		
+		return 0;
+		
+	}
+
+	public int getRetContratante() {
+		return retContratante;
+	}
+
+	public void setRetContratante(int retContratante) {
+		this.retContratante = retContratante;
+	}
+	
+
+}

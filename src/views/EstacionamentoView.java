0f3bd6b0f7c1a006518abd47eb836da15c638542
Vinicius Vieira <vncvieira00@gmail.com>
2023-07-17 12:02:34 -0300

    Manuseio de Erros na View de estacionamento

--- src/views/EstacionamentoView.java
+++ src/views/EstacionamentoView.java
@@ -8,81 +8,139 @@ public class EstacionamentoView {
 
 
 
-  public int lerIdEstacionamento() {
+  public int lerIdEstacionamento() throws IllegalArgumentException{
+    
     System.out.println("Digite o id do estacionamento: ");
+
+    if(sc.nextInt() < 0){
+      throw new IllegalArgumentException("Id inválido");
+    }
     return sc.nextInt();
   }
 
-  public int lerHoraEntrada() {
+  public int lerHoraEntrada() throws IllegalArgumentException{
     System.out.println("Digite a hora de entrada: ");
+
+    if(sc.nextInt() < 0 || sc.nextInt() > 23){
+      throw new IllegalArgumentException("Hora de entrada inválida");
+    }
     return sc.nextInt();
   }
 
-  public int lerMinutoEntrada() {
+  public int lerMinutoEntrada() throws IllegalArgumentException{
     System.out.println("Digite o minuto de entrada: ");
+
+    if(sc.nextInt() < 0 || sc.nextInt() > 59){
+      throw new IllegalArgumentException("Minuto de entrada inválido");
+    }
     return sc.nextInt();
   }
 
-  public int lerHoraSaida() {
+  public int lerHoraSaida() throws IllegalArgumentException{
     System.out.println("Digite a hora de saida: ");
+
+    if(sc.nextInt() < 0 || sc.nextInt() > 23){
+      throw new IllegalArgumentException("Hora de saida inválida");
+    }
     return sc.nextInt();
   }
 
-  public int lerMinutoSaida() {
+  public int lerMinutoSaida() throws IllegalArgumentException{
     System.out.println("Digite o minuto de saida: ");
+
+    if(sc.nextInt() < 0 || sc.nextInt() > 59){
+      throw new IllegalArgumentException("Minuto de saida inválido");
+    }
     return sc.nextInt();
   }
 
-  public String lerPlaca() {
+  public String lerPlaca() throws IllegalArgumentException{
     System.out.println("Digite a placa do veiculo: ");
+
+    if(sc.nextLine().length() < 7 || sc.nextLine().length() > 7){
+      throw new IllegalArgumentException("Placa inválida");
+    }
     return sc.nextLine();
   }
 
-  public String lerEvento() {
+  public String lerEvento() throws IllegalArgumentException{
     System.out.println("Digite o nome do evento: ");
+
+    if(sc.nextLine().length() < 3){
+      throw new IllegalArgumentException("Nome do evento inválido");
+    }
+
     return sc.nextLine();
   }
 
-  public double lerValorEvento() {
+  public double lerValorEvento() throws IllegalArgumentException{
     System.out.println("Digite o valor do evento: ");
+
+    if(sc.nextDouble() < 0){
+      throw new IllegalArgumentException("Valor do evento inválido");
+    }
     return sc.nextDouble();
   }
 
-  public double lerValorMensalista() {
+  public double lerValorMensalista() throws IllegalArgumentException{
     System.out.println("Digite o valor do mensalista: ");
+
+    if(sc.nextDouble() < 0){
+      throw new IllegalArgumentException("Valor do mensalista inválido");
+    }
     return sc.nextDouble();
   }
 
-  public double lerValorNoturno() {
+  public double lerValorNoturno() throws IllegalArgumentException{
     System.out.println("Digite o valor da diaria noturna: ");
+
+    if(sc.nextDouble() < 0){
+      throw new IllegalArgumentException("Valor da diaria noturna inválido");
+    }
     return sc.nextDouble();
   }
 
-  public char lerOpcaoEvento() {
+  public char lerOpcaoEvento() throws IllegalArgumentException{
     System.out.println("Acesso por tipo evento?: (s/n)");
+
+    if(sc.next().charAt(0) != 's' && sc.next().charAt(0) != 'n'){
+      throw new IllegalArgumentException("Opção inválida");
+    }
+
     return sc.next().charAt(0);
   }
 
-  public char lerOpcaoMensalista() {
+  public char lerOpcaoMensalista() throws IllegalArgumentException{
     System.out.println("Acesso por tipo Mensalista?: (s/n)");
+
+    if(sc.next().charAt(0) != 's' && sc.next().charAt(0) != 'n'){
+      throw new IllegalArgumentException("Opção inválida");
+    }
+
     return sc.next().charAt(0);
   }
 
-  public char lerOpcaoNoturno() {
+  public char lerOpcaoNoturno() throws IllegalArgumentException{
     System.out.println("Acesso por tipo Diaria Noturna?: (s/n)");
+
+    if(sc.next().charAt(0) != 's' && sc.next().charAt(0) != 'n'){
+      throw new IllegalArgumentException("Opção inválida");
+    }
+
     return sc.next().charAt(0);
   }
 
-  public void imprimirAcesso() {
+  public void imprimirAcesso() throws IllegalArgumentException{
     System.out.println("Acesso cadastrado com sucesso!");
+    
   }
 
-  public int lerCapacidade() {
+  public int lerCapacidade() throws IllegalArgumentException{
     System.out.println("Digite a capacidade do estacionamento: ");
     return sc.nextInt();
   }
 
-  public void exibirMenuEstacionamentos(EstacionamentoControl estacionamentoControl) {
+  public void exibirMenuEstacionamentos(EstacionamentoControl estacionamentoControl) throws IllegalArgumentException{
     int opcao;
 
     do {
